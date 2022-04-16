f34b1589b5d214581062fccb570cc35951969dfc
Avery King <kavery086@gmail.com>
2022-04-16 00:25:40 -0400

    efficiency optimized

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 function sumIntegers(n) {
     let sum = 0;
     for (let i = 1; i <= n; i++) {
@@ -5,14 +6,19 @@ function sumIntegers(n) {
     }
     return sum;
   }
+*/
+
+  function sumIntegers2(n) {
+    return n * (n + 1) / 2;
+  }
 
   const NUMBER_OF_REPETITIONS = 10; 
-  const N = 100;
+  const N = 10000;
   let sumOfRunningTime = 0n;
   
   for (let k = 1; k <= NUMBER_OF_REPETITIONS; k++) {
     const start = process.hrtime.bigint();
-    const answer = sumIntegers(N);
+    const answer = sumIntegers2(N);
     const end = process.hrtime.bigint();
     sumOfRunningTime += end - start;
   }
